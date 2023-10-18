97b57b3f296657d01455fcdb9081e2e8f0311709
Mahmud Munna <smbmunna@gmail.com>
2023-10-19 00:01:02 +0600

    Brand Create done

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 //Middlewares
 app.use(cors());
 app.use(express.json())
+
+//user cred
+//techshopmaster
+//Ka1MIivLHlcNsAyZ
+const uri = "mongodb+srv://techshopmaster:Ka1MIivLHlcNsAyZ@cluster0.cfuzedb.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        //brands collection
+        const database= client.db('mobileDB');
+        const brandCollection= database.collection('brands');
+
+
+        //post api for insert data
+        app.post('/brands', async(req, res) => {
+            const brand = req.body;
+            console.log(brand);            
+            const result= await brandCollection.insertOne(brand);
+            res.send(result);
+        });
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', async (req, res) => {
     res.send('Tech Garage server has started.');
