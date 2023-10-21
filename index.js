640978fda344e9ce2ad8d5c58235ba78f9fc1865
Mahmud Munna <smbmunna@gmail.com>
2023-10-21 14:35:31 +0600

    cart delete updated

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        //await client.connect();
         //brands collection
         const database= client.db('mobileDB');
         const brandCollection= database.collection('brands');
@@ -113,8 +113,9 @@ async function run() {
         //Delete cart item
         app.delete('/delete/cart/:id', async (req,res)=>{
             const id= req.params.id;
-            console.log(id);
-            const query= {_id:id};
+            //const query= {productID:id};
+            // const query = { userEmail: userEmail, 'product._id': productId };
+            const query = {'product._id': id };
             const result= await cartCollection.deleteOne(query);
             res.send(result);
         })
